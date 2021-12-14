1b80e433a0e0d326a2483114cdb1ccafa15309d1
Gabriel <gabriel.artioli@epiousion.com.br>
2021-12-14 15:33:44 -0300

    Criado projeto

---
+++ pages/Iniciais/iniciais.ts
@@ -0,0 +1,14 @@
+import { Component } from '@angular/core';
+import { NavController } from 'ionic-angular';
+
+@Component({
+  selector: 'page-iniciais',
+  templateUrl: 'iniciais.html'
+})
+export class IniciaisPage {
+
+  constructor(public navCtrl: NavController) {
+
+  }
+
+}
